dc2a96906b99b128ec0ab91b58332d6a8c782233
Akshit Mahaur <mahaurakshit7@gmail.com>
2022-04-19 12:23:56 +0530

    Update MulThread.java
    Assignment Final

--- MulThread.java
+++ MulThread.java
@@ -9,11 +9,11 @@ class M1 extends Thread
 	{
 		try
 		{
-			System.out.println(" Inside run() method isAlive : " + Thread.currentThread().isAlive());
+			System.out.println("  Inside run() method isAlive : " + Thread.currentThread().isAlive());
 		}
 		catch(Exception e)
 		{
-			System.out.println("Exception caught.");
+			System.out.println("  Exception caught.");
 		}
 	}
 }
@@ -25,20 +25,20 @@ class M2 implements Runnable
 	{
 		try
 		{
-			System.out.println("ORIGINAL PRIORITY OF Thread " + Thread.currentThread().getName() + " with priority " + Thread.currentThread().getPriority() + " is running." );
+			System.out.println("  ORIGINAL PRIORITY OF Thread " + Thread.currentThread().getName() + " with priority " + Thread.currentThread().getPriority() + " is running." );
 			
 			if(Thread.currentThread().getPriority() == Thread.MAX_PRIORITY)
 				Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
 			
 			Thread.sleep(5000);
 			
-			System.out.println("NEW PRIORITY OF Thread " + Thread.currentThread().getName() + " with priority " + Thread.currentThread().getPriority() + " is running." );
+			System.out.println("  NEW PRIORITY OF Thread " + Thread.currentThread().getName() + " with priority " + Thread.currentThread().getPriority() + " is running." );
 			
 		}
 		
 		catch(Exception e)
 		{
-			System.out.println("Exception is caught");
+			System.out.println("  Exception is caught");
 		}
 	}
 }
@@ -52,25 +52,25 @@ public class MulThread
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		System.out.println("\n--------------------------------------------\n CLASS M1 \n--------------------------------------------\n");
+		System.out.println("\n--------------------------------------------\n \t\t CLASS M1 \n--------------------------------------------\n");
 		
 		M1 o1 = new M1();
 		M1 o2 = new M1();
 		
 		// using join()
-        System.out.println(" O1 before starting thread isAlive : " + o1.isAlive());   
+        System.out.println("  O1 before starting thread isAlive : " + o1.isAlive());   
         o1.start(); 
         try 
         {
         	o1.join();
         }
         catch(Exception e) {}
-        System.out.println(" O1 after starting thread isAlive : " + o1.isAlive());
+        System.out.println("  O1 after starting thread isAlive : " + o1.isAlive());
         
         // without using join()
-        System.out.println("\n O2 before starting thread isAlive : " + o2.isAlive());   
+        System.out.println("\n  O2 before starting thread isAlive : " + o2.isAlive());   
         o2.start(); 
-        System.out.println(" O2 after starting thread isAlive : " + o2.isAlive());
+        System.out.println("  O2 after starting thread isAlive : " + o2.isAlive());
 		
 		
 		try {
@@ -78,10 +78,10 @@ public class MulThread
 		} catch (Exception e) {}
 		
 		
-		System.out.print("\n--------------------------------------------\nEnter the number of threads : ");
+		System.out.print("\n--------------------------------------------\n  Enter the number of threads : ");
 		int n = Integer.parseInt(in.readLine()); 
 		
-		System.out.println("\n--------------------------------------------\n CLASS M2 \n--------------------------------------------\n ");
+		System.out.println("\n--------------------------------------------\n \t\t CLASS M2 \n--------------------------------------------\n ");
 	    for (int i = 0; i < n; i++) 
 	    {
 	        Thread obj = new Thread(new M2(), ("number " + (i+1)));
